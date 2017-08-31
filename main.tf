81921bbc8cb01f861bba0c58c447f17db4f3a2a6
ckelner <chris.kelner@gmail.com>
2017-08-31 00:09:49 -0400

    Need outout communication

--- main.tf
+++ main.tf
@@ -25,6 +25,12 @@ resource "aws_security_group" "allow_all" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
   tags {
     Name = "allow_all_temp_test"
     "Terraform" = "true"
